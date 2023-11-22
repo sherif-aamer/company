d2bbbc9521d31366b0f3d5dfa221ed0ef45d8c70
unknown <sobhisherrif@gmail.com>
2023-11-22 12:00:19 +0100

    company

---
+++ src/js/index.js
@@ -0,0 +1,135 @@
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+
+$(function(){
+    $('.thumbnail2').hover(function(){
+        $(this).find('.project-category').hide();
+        $(this).find('.caption2').slideDown(250);
+    },
+    function(){
+        $(this).find('.caption2').slideUp(250);
+        $(this).find('.project-category').show();
+    }
+    
+    );
+
+    var pathname = window.location.pathname; // to get the location //
+    console.log(pathname);
+    $('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+
+  if(pathname == "/blog-details.html" || pathname =="/add-blog.html") {
+    $('.navbar-nav > li > a[href="/blog.html"]').parent().addClass('active');
+
+  }
+
+    if(pathname == "/project-details.html") {
+      $('.navbar-nav > li > a[href="/projects.html"]').parent().addClass('active');
+
+  }
+
+    let modalId = $('#image-gallery');
+
+    
+
+    loadGallery(true, 'a.thumbnail');
+
+
+    //This function disables buttons when needed
+    function disableButtons(counter_max, counter_current) {
+      $('#show-previous-image, #show-next-image')
+        .show();
+      if (counter_max === counter_current) {
+        $('#show-next-image')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#show-previous-image')
+          .hide();
+      }
+    }
+
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .on('click',function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+
+
+// build key actions
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37: // left
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39: // right
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+
+})
+
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("date").innerHTML = year;
+
+
+// Add the following code if you want the name of the file appear on select
+$(".custom-file-input").on("change", function() {
+  var fileName = $(this).val().split("\\").pop();
+  $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+});
---
+++ webpack.config.js
@@ -0,0 +1,131 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+
+  entry: './src/js/index.js',
+
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js',
+
+  },
+
+  module: {
+
+    rules: [
+      {
+        test: /\.(sass|css|scss)$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+
+
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: "images",
+            }
+          },
+        ],
+      },
+
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: "fonts",
+            }
+          },
+        ],
+      },
+
+
+
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
+    ],
+
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    // compress: true,
+    port: 9000,
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+
+    new HtmlWebpackPlugin({
+      template: './src/projects.html',
+      filename: 'projects.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/project-details.html',
+      filename: 'project-details.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/blog.html',
+      filename: 'blog.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/blog-details.html',
+      filename: 'blog-details.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/add-blog.html',
+      filename: 'add-blog.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      filename: 'about.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/contact.html',
+      filename: 'contact.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css"
+    })
+  ],
+};
